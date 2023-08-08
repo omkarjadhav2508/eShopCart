ae7e366903eb566012960b7e0b89161c116b048c
omkar <ojadhav857@gmail.com>
2023-08-08 12:56:46 +0530

    Added initial code

---
+++ src/main/java/com/velocity/eShopCart/controller/controller.java
@@ -0,0 +1,16 @@
+package com.velocity.eShopCart.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+	@RequestMapping("/test")
+	public ResponseEntity HelloWorld() {
+		
+		return ResponseEntity.ok().body("Hello World");
+	}
+	
+}
